4088fbbe1221eb055f98fd5199a79936385fa9f8
Ausmarton Zarino Fernandes <ausmarton@gmail.com>
2014-03-02 07:11:42 +0530

    Tryin for port number from the Env first, app.coffee is stil setting port which does not get used anywhere

--- server.js
+++ server.js
@@ -3,7 +3,7 @@ var app, port;
 app = require('./.app/app');
 
 //needs to be moved to some config
-port = 3000;
+port = process.env.PORT || 3000;
 
 app.listen(port, function() {
   return console.log("Listening on " + port + "\nPress CTRL-C to stop server.");
